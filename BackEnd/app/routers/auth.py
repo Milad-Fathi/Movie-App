6e204fcf23bfee1c05e8bd7ee85b461f8952c776
milad fathi <miladfathimf7@gmail.com>
2024-01-06 14:39:17 -0800

    api for admin's sign in process

--- BackEnd/app/routers/auth.py
+++ BackEnd/app/routers/auth.py
@@ -27,7 +27,7 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-class UserRequest(BaseModel):
+class PersonRequest(BaseModel):
     user_name: str = Field(min_length=1)
     plain_text_password: str = Field(min_length=1)
     # role: str = Field(min_length=1)
@@ -37,9 +37,9 @@ class UserRequest(BaseModel):
 
 
 # creating user
-@router.post("/sign-in", status_code=status.HTTP_201_CREATED)
+@router.post("/sign-in_user", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency,
-                      create_user_request: UserRequest):
+                      create_user_request: PersonRequest):
     
     create_user_model = Person(
         email=create_user_request.email,
@@ -53,7 +53,19 @@ async def create_user(db: db_dependency,
 
 
 # creating admin
+@router.post("/sign-in_admin", status_code=status.HTTP_201_CREATED)
+async def create_admin(db: db_dependency,
+                      create_admin_request: PersonRequest):
     
+    create_user_model = Person(
+        email=create_admin_request.email,
+        user_name=create_admin_request.user_name,
+        role="admin",
+        hashed_password=bcrypt_context.hash(create_admin_request.plain_text_password),
+    )
+
+    db.add(create_admin_request)
+    db.commit()
 
 
 # read user
@@ -64,9 +76,7 @@ async def read_user(db: db_dependency,
     user_model = db.query(Person).filter(Person.user_name == user_name).first()
     if (user_model is not None ) and (bcrypt_context.verify(password, user_model.hashed_password)):
         return user_model
-    else:
-        return bcrypt_context.hash(password)
-    # raise HTTPException(status_code=404, detail="user not found")
+    raise HTTPException(status_code=404, detail="user not found")
 
 
 # read admin
