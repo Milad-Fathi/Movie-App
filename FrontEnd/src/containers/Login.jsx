0bfe5bdcafbcce11558c179d3bbf14dfdb9c5706
Asus <moeinpasandide@gmail.com>
2024-01-11 22:35:36 +0330

    login container added

---
+++ FrontEnd/src/containers/Login.jsx
@@ -0,0 +1,10 @@
+import '../index.css'
+const Login = () => {
+    return ( 
+        <div>
+            <div className="bg-red-500">Hello world</div>
+        </div>
+     );
+}
+ 
+export default Login;
