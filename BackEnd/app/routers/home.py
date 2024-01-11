c82da2f740d576b202aa0a4740911520b5335ab7
milad fathi <miladfathimf7@gmail.com>
2024-01-11 12:53:33 -0800

    home page's APIs completed

--- BackEnd/app/routers/home.py
+++ BackEnd/app/routers/home.py
@@ -62,13 +62,13 @@ async def search_movie(db: db_dependency,
 
 
 
-# return genre's id by telling the name of genre
-@router.get("/genre", status_code=status.HTTP_200_OK)
-async def read_genre(db: db_dependency, name_genre):
-    genre_model = db.query(Genre).filter(Genre.name == name_genre).first()
-    if genre_model is not None:
-        return genre_model
-    raise HTTPException(status_code=404, detail="genre not found")
+# # return genre's id by telling the name of genre
+# @router.get("/genre", status_code=status.HTTP_200_OK)
+# async def read_genre(db: db_dependency, name_genre):
+#     genre_model = db.query(Genre).filter(Genre.name == name_genre).first()
+#     if genre_model is not None:
+#         return genre_model
+#     raise HTTPException(status_code=404, detail="genre not found")
 
 
 
